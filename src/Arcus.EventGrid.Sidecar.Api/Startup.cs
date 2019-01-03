448ab342780d6afba85fa994e10cada0af85416e
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-01-03 09:41:16 +0100

    Create minimal API with health endpoint and Open API docs
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Arcus.EventGrid.Sidecar.Api/Startup.cs
@@ -0,0 +1,107 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Arcus.EventGrid.Sidecar.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                    .AddJsonOptions(jsonOptions =>
+                    {
+                        jsonOptions.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+                        jsonOptions.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                    })
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var openApiInformation = new Info
+            {
+                Contact = new Contact
+                {
+                    Name = "Arcus",
+                    Url = "https://github.com/arcus-azure/arcus.eventgrid.sidecar"
+                },
+                Title = "Arcus v1",
+                Description = "Push events to Azure Event Grid in a breeze - API allows you to send events over REST without having to worry about how Azure Event Grid works.",
+                Version = "v1",
+                License = new License
+                {
+                    Name = "MIT",
+                    Url = "https://github.com/arcus-azure/arcus.eventgrid.sidecar/blob/master/LICENSE"
+                }
+            };
+
+            var xmlDocumentationPath = GetXmlDocumentationPath(services);
+
+            services.AddSwaggerGen(swaggerGenerationOptions =>
+            {
+                swaggerGenerationOptions.EnableAnnotations();
+                swaggerGenerationOptions.SwaggerDoc("v1", openApiInformation);
+                swaggerGenerationOptions.DescribeAllEnumsAsStrings();
+
+                if (string.IsNullOrEmpty(xmlDocumentationPath) == false)
+                {
+                    swaggerGenerationOptions.IncludeXmlComments(xmlDocumentationPath);
+                }
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(swaggerUiOptions =>
+            {
+                swaggerUiOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Arcus Event Grid v1 API");
+                swaggerUiOptions.DisplayOperationId();
+                swaggerUiOptions.EnableDeepLinking();
+                swaggerUiOptions.DocumentTitle = "Arcus Event Grid API";
+                swaggerUiOptions.DocExpansion(DocExpansion.List);
+                swaggerUiOptions.DisplayRequestDuration();
+                swaggerUiOptions.EnableFilter();
+            });
+        }
+
+        private static string GetXmlDocumentationPath(IServiceCollection services)
+        {
+            var hostingEnvironment = services.FirstOrDefault(service => service.ServiceType == typeof(IHostingEnvironment));
+            if (hostingEnvironment == null)
+            {
+                return string.Empty;
+            }
+
+            var contentRootPath = ((IHostingEnvironment)hostingEnvironment.ImplementationInstance).ContentRootPath;
+            var xmlDocumentationPath = $"{contentRootPath}/Open-Api-Docs.xml";
+
+            return File.Exists(xmlDocumentationPath) ? xmlDocumentationPath : string.Empty;
+        }
+    }
+}
