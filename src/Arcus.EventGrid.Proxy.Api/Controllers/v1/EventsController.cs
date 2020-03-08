d17c689a620ba24c62dbbce61372a452df955066
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-03-08 15:58:33 +0100

    pr-fix: use cloud events in integration tests

--- src/Arcus.EventGrid.Proxy.Api/Controllers/v1/EventsController.cs
+++ src/Arcus.EventGrid.Proxy.Api/Controllers/v1/EventsController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Arcus.EventGrid.Publishing.Interfaces;
+using CloudNative.CloudEvents;
 using GuardNet;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -38,19 +40,19 @@ namespace Arcus.EventGrid.Proxy.Api.Controllers.v1
         [SwaggerResponse((int) HttpStatusCode.NoContent, Description = "Event was successfully emitted")]
         [SwaggerResponse((int) HttpStatusCode.BadRequest, Description = "The request to emit an event was not valid")]
         [SwaggerResponse((int) HttpStatusCode.InternalServerError, Description = "Unable to emit event due to internal error")]
-        public async Task<IActionResult> Emit(string eventType, [FromBody, Required] object eventPayload, string eventId, string eventTimestamp, string eventSubject = "/", string dataVersion = "1.0")
+        public async Task<IActionResult> Emit(string eventType, [FromBody, Required] string eventPayload, string eventId, string eventTimestamp, string eventSubject = "/", string dataVersion = "1.0")
         {
             eventId = string.IsNullOrWhiteSpace(eventId) ? Guid.NewGuid().ToString() : eventId;
             eventTimestamp = string.IsNullOrWhiteSpace(eventTimestamp) ? DateTimeOffset.UtcNow.ToString(format: "O") : eventTimestamp;
 
-            var rawEventPayload = JsonConvert.SerializeObject(eventPayload);
-
-            if (DateTimeOffset.TryParse(eventTimestamp, out var parsedEventTimeStamp) == false)
+            if (DateTimeOffset.TryParse(eventTimestamp, out DateTimeOffset parsedEventTimeStamp) == false)
             {
                 return BadRequest($"Unable to parse specified event timestamp '{eventTimestamp}'");
             }
 
-            await _eventGridPublisher.PublishRawAsync(eventId, eventType, rawEventPayload, eventSubject, dataVersion, parsedEventTimeStamp);
+            var source = new Uri($"{Request.Scheme}://{Request.Host}{Request.PathBase}");
+            await _eventGridPublisher.PublishRawCloudEventAsync(
+                CloudEventsSpecVersion.V1_0, eventId, eventType, source, eventPayload, eventSubject, parsedEventTimeStamp.DateTime);
 
             Response.Headers.Add(Headers.Response.Events.Id, eventId);
             Response.Headers.Add(Headers.Response.Events.Subject, eventSubject);
